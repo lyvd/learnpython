9980699f82d7b17b5110f78819e2ee3e153252f5
lyvd <vuducly151092@gmail.com>
2016-09-24 18:57:27 +0700

    Tree implementation

--- tree.py
+++ tree.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
-
-
+# source: http://interactivepython.org/runestone/static/pythonds/Trees/ListofListsRepresentation.html
+# construct a tree as list of lists
+# the root node is the fist item, the second item is the left subtree
+# the third item is the right subtree
 myTree = ['a',   #root
       ['b',  #left subtree
        ['d', [], []],
@@ -10,4 +12,21 @@ myTree = ['a',   #root
        [] ]
      ]
 
+# display the tree
 print(myTree)
+
+# the root node
+print(myTree[0])
+
+# the left subtree
+print(myTree[1])
+
+# the right subtree
+print(myTree[2])
+
+# a function to construct a tree
+def BinaryTree(r):
+	return [r, [], []]
+
+def insertLeft(root, newBranch):
+	t = root.pop(1)
